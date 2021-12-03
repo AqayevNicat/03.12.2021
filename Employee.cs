957a8a8898864b849f3798faa5d9b3209ca675ff
AqayevNicat <nijatam@code.edu.az>
2021-12-03 09:46:25 +0400

    Project2-C#

---
+++ Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Employee
+    {
+        public string Name;
+        public string Surname;
+        public string Position;
+        public int DailySalary;
+        public int DayCount = 30;
+
+        public void MonthlySalary()
+        {
+            Console.WriteLine(
+                $" Name : {Name}\n" +
+                $" Surname : {Surname}\n" +
+                $" Position :  {Position}\n" +
+                $" Salary : {DayCount * DailySalary}\n"
+                );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.Name = "Nicat";
+            employee1.Surname = "Aqayev";
+            employee1.Position = "Administrator";
+            employee1.DailySalary = 40;
+            employee1.MonthlySalary();
+
+            Employee employee2 = new Employee();
+            employee2.Name = "John";
+            employee2.Surname = "Lennon";
+            employee2.Position = "Director";
+            employee2.DailySalary = 500;
+            employee2.DayCount = 20;
+            employee2.MonthlySalary();
+        }
+    }
+}
